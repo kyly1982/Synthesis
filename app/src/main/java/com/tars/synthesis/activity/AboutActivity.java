3b3f04dde90caa25b01398b44e30083c41cf300d
kyly1982 <anloy_3633@sohu.com>
2015-11-23 20:10:53 +0800

    init

--- app/src/main/java/com/tars/synthesis/activity/AboutActivity.java
+++ app/src/main/java/com/tars/synthesis/activity/AboutActivity.java
@@ -1,11 +1,8 @@
 package com.tars.synthesis.activity;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 import com.tars.synthesis.R;
 import com.tars.synthesis.utils.AutoUpgrade.AppUpdate;
